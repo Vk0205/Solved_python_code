4b72e7bb95331e0ea4e8b50131f1598859150f14
Vk0205 <108221507+Vk0205@users.noreply.github.com>
2023-06-23 00:05:02 +0530

    Update Add_Two_Numbers.py

--- Add_Two_Numbers.py
+++ Add_Two_Numbers.py
@@ -1,19 +1,19 @@
 class Solution:
-  def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
-    dummy = ListNode(0)
-    curr = dummy
-    carry = 0
-
-    while carry or l1 or l2:
-      if l1:
-        carry += l1.val
-        l1 = l1.next
-      if l2:
-        carry += l2.val
-        l2 = l2.next
-      curr.next = ListNode(carry % 10)
-      carry //= 10
-      curr = curr.next
-
-    return dummy.next
-
+    def addTwoNumbers(self, l1, l2):
+        carry = 0
+        # dummy head
+        head = curr = ListNode(0)
+        while l1 or l2:
+            val = carry
+            if l1:
+                val += l1.val
+                l1 = l1.next
+            if l2:
+                val += l2.val
+                l2 = l2.next
+            curr.next = ListNode(val % 10)
+            curr = curr.next
+            carry = int(val / 10)
+        if carry > 0:
+            curr.next = ListNode(carry)
+        return head.next
